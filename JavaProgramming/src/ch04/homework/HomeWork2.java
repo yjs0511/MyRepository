9d33dc6275f5c394c8b9b468265a09bc1167fd82
yjs <yjs@112.169.233.74>
2016-08-18 21:52:18 +0900

    과제 수정

--- JavaProgramming/src/ch04/homework/HomeWork2.java
+++ JavaProgramming/src/ch04/homework/HomeWork2.java
@@ -3,14 +3,14 @@ package ch04.homework;
 public class HomeWork2 {
 
 	public static void main(String[] args) {
-		for(int i=0; i<100; ){
+		/*for(int i=0; i<100; ){
 			int x = (int)(Math.random()*10)+1;
 			int y = (int)(Math.random()*10)+1;
 			
 			if((4*x + 5*y) == 60){
 				System.out.println("("+x+", "+y+ ")");
 			}
-		}
+		}*/
 		
 		/*for(int x=1; x<=10; x++){
 			for(int y=1; y<=10; y++){
@@ -19,5 +19,79 @@ public class HomeWork2 {
 				}
 			}
 		}*/
+		
+		int x = 1;
+
+		switch (x) {
+		case 1:
+			for (int y = 1; y <= 10; y++) {
+				if ((4 * x + 5 * y) == 60) {
+					System.out.println("(" + x + ", " + y + ")");
+				}
+			}
+			
+		case 2:
+			for (int y = 1; y <= 10; y++) {
+				if ((4 * x + 5 * y) == 60) {
+					System.out.println("(" + x + ", " + y + ")");
+				}
+			}
+			
+		case 3:
+			for (int y = 1; y <= 10; y++) {
+				if ((4 * x + 5 * y) == 60) {
+					System.out.println("(" + x + ", " + y + ")");
+				}
+			}
+			
+		case 4:
+			for (int y = 1; y <= 10; y++) {
+				if ((4 * x + 5 * y) == 60) {
+					System.out.println("(" + x + ", " + y + ")");
+				}
+			}
+			
+		case 5:
+			for (int y = 1; y <= 10; y++) {
+				if ((4 * x + 5 * y) == 60) {
+					System.out.println("(" + x + ", " + y + ")");
+				}
+			}
+			
+		case 6:
+			for (int y = 1; y <= 10; y++) {
+				if ((4 * x + 5 * y) == 60) {
+					System.out.println("(" + x + ", " + y + ")");
+				}
+			}
+			
+		case 7:
+			for (int y = 1; y <= 10; y++) {
+				if ((4 * x + 5 * y) == 60) {
+					System.out.println("(" + x + ", " + y + ")");
+				}
+			}
+			
+		case 8:
+			for (int y = 1; y <= 10; y++) {
+				if ((4 * x + 5 * y) == 60) {
+					System.out.println("(" + x + ", " + y + ")");
+				}
+			}
+			
+		case 9:
+			for (int y = 1; y <= 10; y++) {
+				if ((4 * x + 5 * y) == 60) {
+					System.out.println("(" + x + ", " + y + ")");
+				}
+			}
+			
+		case 10:
+			for (int y = 1; y <= 10; y++) {
+				if ((4 * x + 5 * y) == 60) {
+					System.out.println("(" + x + ", " + y + ")");
+				}
+			}
+		}
 	}
 }
