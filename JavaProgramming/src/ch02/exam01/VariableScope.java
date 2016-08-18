dddd6dc8880b7e649a4be9c97ddb608c904a1e23
yjs0511 <hypermega22@gmail.com>
2016-08-18 11:07:42 +0900

    수정했음

--- JavaProgramming/src/ch02/exam01/VariableScope.java
+++ JavaProgramming/src/ch02/exam01/VariableScope.java
@@ -4,7 +4,7 @@ public class VariableScope {
 	// 클래스는 첫자는 대문자 다음 단어도 첫자는 대문자가 관례 (파일명과 동일)
 	public static void main(String[] ar) {
 		int var1 = 10;
-		System.out.println("var1 = " + var1);
+		System.out.println("var1= " + var1);
 		// '+' 는 숫자와 문자열을 결함하는데 사용되기도 한다.
 
 		if (true) {
